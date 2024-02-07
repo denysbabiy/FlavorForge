2b6f631e9d8ebfc81cca290708b44030db9828b8
Denys Babii <denys_babii@epam.com>
2024-02-07 14:42:39 +0200

    Initial commit

---
+++ src/main/java/org/flavorforge/flavorforge/FlavorForgeApplication.java
@@ -0,0 +1,13 @@
+package org.flavorforge.flavorforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavorForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlavorForgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/flavorforge/flavorforge/FlavorForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.flavorforge.flavorforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlavorForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
